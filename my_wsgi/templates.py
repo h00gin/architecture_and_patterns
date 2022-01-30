bedc99b255ee09ef7b31d482c8f93b2e5931149c
h00gin <h00gin@yandex/ru>
2022-01-30 21:16:58 +0300

    lesson_3 completed

--- my_wsgi/templates.py
+++ my_wsgi/templates.py
@@ -1,12 +1,10 @@
-import os
-
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 
 def render(template_name, folder='templates', **kwargs):
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
 
 
